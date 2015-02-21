e3bac964e2092e1f78022746548b30f7a5e9d19b
Jonathan Davies <jpdavs@gmail.com>
2015-02-21 10:44:14 +0000

    wiki_spec.rb: Check wiki configuration file contents.

--- spec/defines/wiki_spec.rb
+++ spec/defines/wiki_spec.rb
@@ -13,12 +13,29 @@ describe 'moinmoin::wiki', :type => :define do
         :sitename            => 'Wiki',
         :interwikiname       => 'Wiki',
         :data_dir            => '/srv/wiki/data/',
-        :data_underlay_dir   => '/srv/wiki/underlay',
+        :data_underlay_dir   => '/srv/wiki/underlay/',
         :httpd_external_auth => false,
       }  
     end
 
-    it { should contain_file('/etc/moin/wiki.py') }
+    it { should contain_class('moinmoin::params') }
+
+    it do
+      should contain_file('/etc/moin/wiki.py') \
+        .with_content(/^    sitename = u'Wiki' # \[Unicode\]$/)
+    end
+    it do
+      should contain_file('/etc/moin/wiki.py') \
+        .with_content(/^    interwikiname = u'Wiki' # \[Unicode\]$/)
+    end
+    it do
+      should contain_file('/etc/moin/wiki.py') \
+        .with_content(/^    data_dir = '\/srv\/wiki\/data\/'$/)
+    end
+    it do
+      should contain_file('/etc/moin/wiki.py') \
+        .with_content(/^    data_underlay_dir = '\/srv\/wiki\/underlay\/'$/)
+    end
     it { should contain_file('/srv/wiki/data/') }
     it { should contain_file('/srv/wiki/underlay/') }
   end
